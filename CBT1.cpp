4fdd4c1cb63a38164f96305e11184e226ccbf4fd
Jawed <ansarimdjawed803@gmail.com>
2026-09-19 11:35:12 +0530

    Reverse the array

--- CBT1.cpp
+++ CBT1.cpp
@@ -14,6 +14,14 @@ int main(){
         cout<<arr[i]<<" ";
     } //normal hm print krre
 
+    cout<<endl;
+
+    //reverse main krne ke liye
+
+    for(int i=n-1;i>=0;i--){
+        cout<<arr[i]<<" ";
+    }
+
 
 
     return 0;
